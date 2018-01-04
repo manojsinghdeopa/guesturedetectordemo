6ccab8359603199f857142c098dddf37c1426545
Manoj Singh <immanojsingh007@gmail.com>
2018-01-04 20:51:54 +0530

    initial comitt..

---
+++ app/src/main/java/com/example/manoj/guesturedetectordemo/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.manoj.guesturedetectordemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements  GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
+
+    private static final String DEBUG_TAG = "Gestures";
+    private GestureDetectorCompat mDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDetector = new GestureDetectorCompat(this,this);
+        // Set the gesture detector as the double tap
+        // listener.
+        mDetector.setOnDoubleTapListener(this);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+
+        Toast.makeText(this, "OnDown", Toast.LENGTH_SHORT).show();
+        
+        Log.d(DEBUG_TAG,"onDown: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent1, MotionEvent motionEvent2, float velocityX, float velocityY) {
+
+        if(motionEvent1.getY() - motionEvent2.getY() > 50){
+
+            Toast.makeText(MainActivity.this , " Swipe Up " , Toast.LENGTH_LONG).show();
+
+            return true;
+        }
+
+        if(motionEvent2.getY() - motionEvent1.getY() > 50){
+
+            Toast.makeText(MainActivity.this , " Swipe Down " , Toast.LENGTH_LONG).show();
+
+            return true;
+        }
+
+        if(motionEvent1.getX() - motionEvent2.getX() > 50){
+
+            Toast.makeText(MainActivity.this , " Swipe Left " , Toast.LENGTH_LONG).show();
+
+            return true;
+        }
+
+        if(motionEvent2.getX() - motionEvent1.getX() > 50) {
+
+            Toast.makeText(MainActivity.this, " Swipe Right ", Toast.LENGTH_LONG).show();
+
+            return true;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Toast.makeText(this, "OnLongPress", Toast.LENGTH_SHORT).show();
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX, float distanceY) {
+        Toast.makeText(this, "OnScroll", Toast.LENGTH_SHORT).show();
+        Log.d(DEBUG_TAG, "onScroll: " + event1.toString() + event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        Toast.makeText(this, "OnShowPress", Toast.LENGTH_SHORT).show();
+        Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        Toast.makeText(this, "SingleTapUp", Toast.LENGTH_SHORT).show();
+        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        Toast.makeText(this, "OnDoubleTap", Toast.LENGTH_SHORT).show();
+        Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        Toast.makeText(this, "onDoubleTapEvent", Toast.LENGTH_SHORT).show();
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        Toast.makeText(this, "onSingleTapConfirmed", Toast.LENGTH_SHORT).show();
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
+        return true;
+    }
+
+}
+
